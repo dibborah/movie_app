099a8ba64a74c94ef322b7fa77511cd9b3950d45
dibborah <90625406+dibborah@users.noreply.github.com>
2023-09-20 04:16:27 +0530

    Add files via upload

---
+++ images.js
@@ -0,0 +1,20 @@
+const images = [
+    {
+        src : "./img/Talk to me.jpg"
+    },
+    {
+        src : "./img/barbie.jpg"
+    },
+    {
+        src : "./img/fast x.jpg"
+    },
+    {
+        src : "../img/MEG 2.jpg"
+    },
+    {
+        src : "../img/Nun 2.jpg"
+    },
+    {
+        src : "../img/Kandahar.jpg"
+    }
+]
